2635047d1ad753bbbc39c575dd6c01f1d56a4738
Tim Clark <tim.clark@propylon.com>
2015-11-30 13:23:15 -0600

    Add faster python solution to 92 (I get 25 seconds)

--- 92-square-digit-chains/square_digit_chains.py
+++ 92-square-digit-chains/square_digit_chains.py
@@ -12,7 +12,7 @@ def one_or_89(n):
     return n
 
 
-def main():
+def slow():
     count = 0
     for n in xrange(1, 10000000):
         if one_or_89(n) == 89:
@@ -20,5 +20,25 @@ def main():
     print count
 
 
+def one_or_89_with_cache(n, cache):
+    path = []
+    while True:
+        if n in cache:
+            for num in path:
+                cache[num] = cache[n]
+            return cache[n]
+        path.append(n)
+        n = square_digits(n)
+
+
+def fast():
+    solution_cache = {1: 1, 89: 89}
+    count = 0
+    for n in xrange(1, 10000000):
+        if one_or_89_with_cache(n, solution_cache) == 89:
+            count += 1
+    print count
+    
+
 if __name__ == "__main__":
-    main()
+    fast()
