0b1749d7667b48dde5d620f3161f54e1aeab78f7
Tim Clark <timeeeee@gmail.com>
2016-10-05 01:13:32 -0400

    finish scheme solution

--- 1-multiples-of-3-and-5/main.scm
+++ 1-multiples-of-3-and-5/main.scm
@@ -1,8 +1,16 @@
-; Sum of all numbers less than 1000 that are divisble by 3 or 5
+; Sum of all numbers less than 1000 that are divisible by 3 or 5
+(use srfi-1)
+
 (define (range start until)
   (if (= start until)
       '()
       (cons start (range (+ start 1) until))))
 
-(display (range 1 1000))
+(define (divisible-by x n) (= (modulo x n) 0))
+
+(display
+ (foldr + 0
+	(filter (lambda (x) (or (divisible-by x 3)
+				(divisible-by x 5)))
+		(range 1 1000))))
 (newline)
